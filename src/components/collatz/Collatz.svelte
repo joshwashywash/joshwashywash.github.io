3b1b776c31f4a2e40c5b5c0a6f0f50ca92bcde00
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-05-07 18:26:31 -0700

    add collatz draft

---
+++ src/components/collatz/Collatz.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { collatzSequence, turtle } from './util';
+
+	export let n = 1;
+
+	const sequences = Array.from({ length: n }, (_, i) =>
+		collatzSequence(i + 1).reverse()
+	);
+
+	const size = 1000;
+	const scale = 0.8;
+
+	const amount = size * scale * (2 / 100);
+	const thickness = size * (1 / 100);
+</script>
+
+<canvas
+	class="w-full max-w-max"
+	use:turtle={{
+		angle: Math.PI / 6,
+		amount,
+		colors(i) {
+			return `hsl(${i ?? 0}, 100%, 75%, 25%)`;
+		},
+		sequences,
+		size,
+		thickness,
+	}}
+/>
---
+++ src/components/collatz/util.ts
@@ -0,0 +1,65 @@
+import type { Action } from 'svelte/action';
+
+const collatz = (n: number) => (n % 2 === 0 ? n / 2 : 3 * n + 1);
+
+export const collatzSequence = (n: number): number[] => {
+	const sequence = [n];
+	while (n !== 1) {
+		n = collatz(n);
+		sequence.push(n);
+	}
+	return sequence;
+};
+
+/**
+ * sequence has the shape [1, ..., n] where n is the starting number of the sequence
+ */
+export const turtle: Action<
+	HTMLCanvasElement,
+	{
+		amount: number;
+		angle: number;
+		colors: (index?: number) => string;
+		sequences: number[][];
+		size: number;
+		thickness: number;
+	}
+> = (
+	canvas,
+	options = {
+		amount: 5,
+		angle: Math.PI / 6,
+		colors() {
+			return 'black';
+		},
+		sequences: [],
+		size: 300,
+		thickness: 5,
+	}
+) => {
+	const context = canvas.getContext('2d');
+	if (context) {
+		canvas.width = options.size;
+		canvas.height = options.size;
+		context.lineWidth = options.thickness;
+		context.lineCap = 'round';
+		context.lineJoin = 'round';
+		let i = 0;
+		for (const sequence of options.sequences) {
+			context.strokeStyle = options.colors(i);
+			context.translate(canvas.width / 2, canvas.height / 2);
+			context.beginPath();
+			context.moveTo(0, 0);
+			for (const n of sequence) {
+				n % 2 === 0
+					? context.rotate(options.angle)
+					: context.rotate(-options.angle);
+				context.lineTo(options.amount, 0);
+				context.translate(options.amount, 0);
+			}
+			context.stroke();
+			context.resetTransform();
+			i += 1;
+		}
+	}
+};
