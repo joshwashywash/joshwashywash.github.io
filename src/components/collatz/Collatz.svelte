9cef9d307ab0aa2c1e6e967458b792efa8f4e7c6
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-05-08 21:17:19 -0700

    use svg instead for responsiveness

--- src/components/collatz/Collatz.svelte
+++ src/components/collatz/Collatz.svelte
@@ -1,29 +1,43 @@
 <script lang="ts">
-	import { collatzSequence, turtle } from './util';
+	import { collatzSequence } from './util';
 
 	export let n = 1;
-
-	const sequences = Array.from({ length: n }, (_, i) =>
-		collatzSequence(i + 1).reverse()
-	);
-
-	const size = 1000;
-	const scale = 0.6;
+	export let size = 10;
+	export let scale = 0.7;
 
 	const amount = size * scale * (3 / 100);
-	const thickness = size * (1 / 100);
+
+	const path = (size: number, amount: number, angle: number) => {
+		const center = size / 2;
+		const angles = [angle, -angle];
+		return (sequence: number[]) => {
+			let d = `m${center}, ${center}`;
+			let a = 0;
+			for (let i = sequence.length - 1; i >= 0; i -= 1) {
+				a += angles[sequence[i] & 1];
+				d += `l${amount * Math.cos(a)}, ${amount * Math.sin(a)}`;
+			}
+			return d;
+		};
+	};
+
+	const pather = path(size, amount, Math.PI / 6);
+
+	const ds = Array.from({ length: n }, (_, i) =>
+		pather(collatzSequence(n - i))
+	);
 </script>
 
-<canvas
-	class="w-full"
-	use:turtle={{
-		angle: Math.PI / 6,
-		amount,
-		color(i) {
-			return `hsl(${i ?? 0}, 100%, 75%, 50%)`;
-		},
-		sequences,
-		size,
-		thickness,
-	}}
-/>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {size} {size}">
+	<g
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		fill="none"
+		stroke-width="1%"
+	>
+		{#each ds as d, i}
+			{@const stroke = `hsl(${i} 100% 50%)`}
+			<path {stroke} {d} />
+		{/each}
+	</g>
+</svg>
