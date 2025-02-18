479d8f5d9c345d8833b3dcbec32265e22be7942d
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2025-02-17 20:19:32 -0600

    fix RoundedPlaneGeomety points

--- src/blog/math/rounded-rectangle/RoundedPlaneGeometry.ts
+++ src/blog/math/rounded-rectangle/RoundedPlaneGeometry.ts
@@ -15,9 +15,6 @@ class RoundedPlaneGeometry extends BufferGeometry {
 		point_count = 32,
 	} = {}) {
 		super();
-		const corner_diameter = 2 * corner_radius;
-		const outer_width = inner_width + corner_diameter;
-		const outer_height = inner_height + corner_diameter;
 
 		const index: number[] = [];
 		const uv: number[] = [0.5, 0.5];
@@ -27,17 +24,21 @@ class RoundedPlaneGeometry extends BufferGeometry {
 		const segment = (2 * Math.PI) / point_count;
 		for (let i = 0; i <= point_count; i += 1) {
 			const amount = segment * (i + 1);
+
 			const c = Math.cos(amount);
 			const s = Math.sin(amount);
+
 			const uvx = 0.5 * (1 + c);
 			const uvy = 0.5 * (1 + s);
+			uv.push(uvx, uvy);
+
 			let x = corner_radius * c;
 			let y = corner_radius * s;
-			x += Math.sign(x) * outer_width;
-			y += Math.sign(y) * outer_height;
+			x += Math.sign(x) * inner_width;
+			y += Math.sign(y) * inner_height;
 			position.push(x, y, 0);
+
 			normal.push(0, 0, 1);
-			uv.push(uvx, uvy);
 			index.push(i, i + 1, 0);
 		}
 
