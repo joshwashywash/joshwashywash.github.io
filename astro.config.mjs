ab6a0852f291d3e68ddacfbe6aa53e4cb7bd112c
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2025-03-05 13:09:16 -0600

    normalize astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
+import expressiveCode from "astro-expressive-code";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import expressiveCode from "astro-expressive-code";
-
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
