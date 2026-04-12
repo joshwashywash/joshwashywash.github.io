1c46b0fed1224910170e32a42b4720a10c49ff04
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2026-04-12 09:48:40 -0500

    simplify math

--- src/blog/math/map-range/map.ts
+++ src/blog/math/map-range/map.ts
@@ -1,5 +1,5 @@
 /**
- * linearly maps a number from the input range [a, b] to the output range [c, d]
+ * returns a function that linearly maps a number in the input range [a, b] to the output range [c, d]
  */
 export default (a: number, b: number, c: number, d: number) => {
 	return (n: number): number => {
