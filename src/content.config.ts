5970380ac633eb1e139f88eae6243779408f7f63
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2026-04-12 15:34:42 -0500

    make use_katex a frontmatter prop

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,10 +4,14 @@ import { z } from "astro/zod";
 
 const schema = z.object({
 	description: z.string(),
+	image_url: z.string().default("/me.jpg"),
+	uses_katex: z.boolean().default(false),
 	is_draft: z.boolean().default(false),
 	published_date: z.date().transform((s) => new Date(s)),
 	title: z.string(),
 });
+
+type T = z.infer<typeof schema>;
 
 const loader = glob({
 	base: "./src/blog",
